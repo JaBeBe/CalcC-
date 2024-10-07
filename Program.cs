7f14ddfb3c0ed18b961cad3281501f2c4ef6c60b
JaBeBe <jacek.a.bebak@gmail.com>
2024-10-07 13:20:57 +0200

    ending

--- Program.cs
+++ Program.cs
@@ -1,4 +1,58 @@
-﻿
-while true{
+﻿using System;
+using System.Data;
+using System.Net;
+
+// declaation of global variables
+bool flagProg = true;
+List<string> MathQuest = new List<string>();
+Menu();
+while (flagProg)
+{
+    var quest = Console.ReadLine();
+    EndingClearProtocol(ref flagProg, ref MathQuest, quest);
+    string resolve = $"{quest} = {ResolveQuestProblem(quest)}";
+    MathQuest.Add(resolve);
+    Console.Clear();
+    foreach (var q in MathQuest)
+    {
+        Console.WriteLine(q);
+    }
+    System.Console.WriteLine("________________");
+}
+
+void Menu()
+{
+    System.Console.WriteLine($"################################");
+    System.Console.WriteLine($"# wpisz działanie matematyczne #");
+    System.Console.WriteLine($"# C - by wykasować historie    #");
+    System.Console.WriteLine($"# E - by zakończyć             #");
+    System.Console.WriteLine($"################################");
+}
+void EndingClearProtocol(ref bool flagProg, ref List<string> MathQuest, string quest)
+{
+    if (quest.Length == 1)
+    {
+        switch (quest.ToLowerInvariant())
+        {
+            case "c":
+                MathQuest.Clear();
+                break;
+            case "e":
+                flagProg = false;
+                break;
+            default:
+                break;
+        }
+    }
+}
+string ResolveQuestProblem(string quest = "")
+{
+    if (quest.Length > 1)
+    {
+        var resolve = new DataTable().Compute(quest, null);
+        return resolve.ToString();
+    }
+    else { return quest; }
+
 
 }
